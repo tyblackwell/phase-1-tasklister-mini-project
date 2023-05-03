0952175e6aaf12948bc223669f2cbf7a550cfce7
Ty Blackwell <tyeverettblackwell@gmail.com>
2023-05-03 16:01:08 -0400

    commit tree 898287ebc4157f82eb05db84f15f33f0de696e90

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+//   const taskForm = document.querySelector("#create-task-form")
+//   taskForm.addEventListener("submit", (event) => {
+//     event.preventDefault()
+//     const newTaskDescription = document.querySelector("#new-task-description")
+//     const newTask = document.createElement("li")
+//     newTask.innerText = newTaskDescription.value
+
+//     const taskList = document.querySelector("#tasks")
+//     taskList.appendChild(newTask)
+
+//     event.target.reset()
+//   })
+// });
+
+// delete functionality
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.querySelector("#create-task-form");
+  const taskList = document.querySelector("#tasks");
+
+  taskForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const newTaskDescription = document.querySelector("#new-task-description");
+    const newTask = document.createElement("li");
+    newTask.innerText = newTaskDescription.value;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    newTask.appendChild(deleteButton);
+
+    taskList.appendChild(newTask);
+
+    event.target.reset();
+  });
+
+  taskList.addEventListener("click", (event) => {
+    if (event.target.tagName === "BUTTON") {
+      deleteTask(event.target.parentElement);
+    }
+  });
+
+  function deleteTask(task) {
+    task.remove();
+  }
 });
